df5c4921f1840b7534a6df52e35c31b9a5c83a12
Amine Smahi <mohammed.amine.smahi@gmail.com>
2018-10-19 22:20:37 +0100

    Add block classe

---
+++ Block.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlockchainCore
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        private DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public IList<Transaction> Transactions { get; set; }
+        private int Nonce { get; set; }
+
+        public Block(DateTime timeStamp, string previousHash, IList<Transaction> transactions)
+        {
+            Index = 0;
+            TimeStamp = timeStamp;
+            PreviousHash = previousHash;
+            Transactions = transactions;
+        }
+
+        public string CalculateHash()
+        {
+            var sha256 = SHA256.Create();
+
+            var inputBytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{JsonConvert.SerializeObject(Transactions)}-{Nonce}");
+            var outputBytes = sha256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        public void Mine(int difficulty)
+        {
+            var leadingZeros = new string('0', difficulty);
+            while (Hash == null || Hash.Substring(0, difficulty) != leadingZeros)
+            {
+                Nonce++;
+                Hash = CalculateHash();
+            }
+        }
+    }
+}
