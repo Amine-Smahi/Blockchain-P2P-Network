002dc233ab514f2f1c340229773aa3af91cbd6d6
Amine Smahi <mohammed.amine.smahi@gmail.com>
2018-10-19 22:20:09 +0100

    Add project structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlockchainCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
