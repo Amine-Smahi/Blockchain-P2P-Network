14fa712dd366827951156fc7ea0c12b73d6abe4f
Amine Smahi <mohammed.amine.smahi@gmail.com>
2018-10-19 22:22:35 +0100

    Add the blockchain classe

---
+++ Blockchain.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlockchainCore
+{
+    public class Blockchain
+    {
+        public IList<Transaction> PendingTransactions = new List<Transaction>();
+        public IList<Block> Chain { set;  get; }
+        private int Difficulty { set; get; } = 2;
+        private int _reward = 1; //1 cryptocurrency
+
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+            AddGenesisBlock();
+        }
+
+        private Block CreateGenesisBlock()
+        {
+            Block block = new Block(DateTime.Now, null, PendingTransactions);
+            block.Mine(Difficulty);
+            PendingTransactions = new List<Transaction>();
+            return block;
+        }
+
+        private void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+
+        private Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+
+        public void CreateTransaction(Transaction transaction)
+        {
+            PendingTransactions.Add(transaction);
+        }
+        public void ProcessPendingTransactions(string minerAddress)
+        {
+            Block block = new Block(DateTime.Now, GetLatestBlock().Hash, PendingTransactions);
+            AddBlock(block);
+
+            PendingTransactions = new List<Transaction>();
+            CreateTransaction(new Transaction(null, minerAddress, _reward));
+        }
+
+        private void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            //block.Hash = block.CalculateHash();
+            block.Mine(Difficulty);
+            Chain.Add(block);
+        }
+
+        public bool IsValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public int GetBalance(string address)
+        {
+            int balance = 0;
+
+            for (int i = 0; i < Chain.Count; i++)
+            {
+                for (int j = 0; j < Chain[i].Transactions.Count; j++)
+                {
+                    var transaction = Chain[i].Transactions[j];
+
+                    if (transaction.FromAddress == address)
+                    {
+                        balance -= transaction.Amount;
+                    }
+
+                    if (transaction.ToAddress == address)
+                    {
+                        balance += transaction.Amount;
+                    }
+                }
+            }
+
+            return balance;
+        }
+    }
+}
